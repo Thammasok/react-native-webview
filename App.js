2c1a7179ee92a3d36a6060e7770e724fc7e203a8
Jaranchai-Surface <jaranchai.nt@hotmail.com>
2020-01-21 18:19:34 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextInput, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function App() {
+  const [text, setText] = useState('');
+
   return (
-    <WebView source={{ uri: 'https://expo.io' }} style={{ marginTop: 20 }} />
+    <View style={{ padding: 10 }}>
+      <TextInput
+        style={{ height: 40, top: 50 }}
+        placeholder="url"
+        onChangeText={text => setText(text)}
+        value={text}
+      />
+
+      <WebView source={{ uri: text }} style={{ marginTop: 20 }} />;
+    </View>
   );
 }
